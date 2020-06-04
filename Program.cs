c91e84f80fd4c0ea15e3635dc02b7e731de96f18
Jainesh1257 <56817512+Jainesh1257@users.noreply.github.com>
2020-06-04 12:16:21 +0530

    Initial Commit - Base code

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace sampleapp
+{
+    class Program
+    {
+        static string ServerName = "Your Server Name";
+        static string DatabaseName = "Your Database Name";
+        
+        static void Main(string[] args)
+        {
+            SqlConnection tmpConn;
+            string sqlCreateDBQuery;
+            tmpConn = new SqlConnection();
+            tmpConn.ConnectionString = "SERVER = " + ServerName + "; Trusted_Connection=True;";
+            sqlCreateDBQuery = " CREATE DATABASE " + DatabaseName;
+
+            SqlCommand myCommand = new SqlCommand(sqlCreateDBQuery, tmpConn);
+            try
+            {
+                tmpConn.Open();
+                Console.WriteLine(sqlCreateDBQuery);
+                var result = myCommand.ExecuteNonQuery();
+                Console.WriteLine(result);
+                Console.WriteLine("Database has been created successfully!");
+
+                if (result == -1)
+                {
+                    RunScript(DatabaseName);
+                }
+                tmpConn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public static void RunScript(string dbName)
+        {
+            SqlConnection tmpConn;
+            tmpConn = new SqlConnection();
+            try
+            {                
+                FileInfo file = new FileInfo("C:\\Users\\ADMIN\\Desktop\\WFHDocuments\\DbScripts\\Generate schema\\Script.sql");
+                string script = file.OpenText().ReadToEnd();
+
+                script = script.Replace("GO", "");
+                script = Regex.Replace(script, "([/*][*]).*([*][/])", "");
+                script = Regex.Replace(script, "\\s{2,}", " ");                
+
+                tmpConn.ConnectionString = "Server=LAPTOP-PRVUC1QQ\\SQLEXPRESS;" + "Database=" + dbName + "; Trusted_Connection=True;";
+                
+
+                using (SqlConnection connection = new SqlConnection(tmpConn.ConnectionString))
+                {
+                    Server server = new Server(new ServerConnection(connection));
+
+                    try
+                    {
+                        server.ConnectionContext.ExecuteNonQuery(script);
+                    }
+                    catch (Exception e)
+                    {
+                        throw e;
+                    }
+
+                }
+            catch (Exception ex)
+            {                
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+    }
+}
